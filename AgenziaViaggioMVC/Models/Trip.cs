fdcc9a120d53d2bf5d4b38366eed35acf109db75
Redux234 <lorenzo.gaetano2002@gmail.com>
2023-01-16 15:32:25 +0100

    Error Message Numerici

--- AgenziaViaggioMVC/Models/Trip.cs
+++ AgenziaViaggioMVC/Models/Trip.cs
@@ -16,14 +16,14 @@
         [EndsWith(".png", ".jpg", ".jpeg", ".webp")]
         public string ImageUrl { get; set; }
 
-        [Range(0, int.MaxValue)]
-        []
+        [Range(0, int.MaxValue, ErrorMessage = "La durata del viaggio non può avere un valore negativo")]
+        
         public int DaysDuration { get; set; }
 
-        [Range(0, int.MaxValue)]
+        [Range(0, int.MaxValue, ErrorMessage = "Il viaggio non può avere un numero di destinazioni negativo")]
         public int Destinations { get ; set; }
 
-        [Range(0, double.MaxValue)]
+        [Range(0, double.MaxValue, ErrorMessage = "Il prezzo della vacanza non può essere negativo")]
         public double CostPerPerson { get; set; }
     }
 }
