150f2d0fdcbb0c8d679c5e08822f3114095b4e76
Thomas Youssef <thomasyoussef.business@protonmail.com>
2023-01-16 12:15:48 +0100

    Add project files.

---
+++ AgenziaViaggioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace AgenziaViaggioMVC.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
