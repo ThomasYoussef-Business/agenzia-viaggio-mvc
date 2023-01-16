0bdb9bcd25d9cd684c7ffd0daa135c495a925bd6
Redux234 <lorenzo.gaetano2002@gmail.com>
2023-01-16 14:31:55 +0100

    Controller base

---
+++ AgenziaViaggioMVC/Controllers/AgencyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AgenziaViaggioMVC.Database;
+using AgenziaViaggioMVC.Models;
+using System.Diagnostics;
+
+
+
+namespace AgenziaViaggioMVC.Controllers
+{
+    public class AgencyController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            using (AgencyContext db = new AgencyContext())
+            {
+                List<Trip> TripList = db.Trips.ToList<Trip>();
+                return View("Index", TripList);
+            }
+
+        }
+    }
+}
--- AgenziaViaggioMVC/Database/AgencyContext.cs
+++ AgenziaViaggioMVC/Database/AgencyContext.cs
@@ -1,6 +1,9 @@
-﻿namespace AgenziaViaggioMVC.Models {
-    public class AgencyContext : DbContext {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+﻿namespace AgenziaViaggioMVC.Database
+{
+    public class AgencyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
             optionsBuilder.UseSqlServer("Data Source=localhost;"
                                         + "Database=AgenziaViaggioMVC;"
                                         + "Integrated Security=True;"
