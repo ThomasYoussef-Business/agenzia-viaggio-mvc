bc5c4a1b1840b49495c2541b7093dab9d43b98c5
Thomas Youssef <thomasyoussef.business@protonmail.com>
2023-01-16 15:36:44 +0100

    Aggiunta prima Migration

---
+++ AgenziaViaggioMVC/Migrations/20230116143551_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgenziaViaggioMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trip",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    DaysDuration = table.Column<int>(type: "int", nullable: false),
+                    Destinations = table.Column<int>(type: "int", nullable: false),
+                    CostPerPerson = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trip", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trip");
+        }
+    }
+}
