9e19ebd6bdc38d6846ebab93421f2903fb753d0c
Thomas Youssef <thomasyoussef.business@protonmail.com>
2023-01-16 15:31:45 +0100

    Aggiunti altri messaggi di errore al modello

--- AgenziaViaggioMVC/Models/Trip.cs
+++ AgenziaViaggioMVC/Models/Trip.cs
@@ -11,16 +11,17 @@
         [MaxLength(256, ErrorMessage = "La descrizione non può essere più lunga di 256 caratteri")]
         public string Description { get; set; }
 
-        [MaxLength(128)]
-        [StartsWith("https://", "./img/")]
-        [EndsWith(".png", ".jpg", ".jpeg", ".webp")]
+        [MaxLength(128, ErrorMessage = "L'URL dell'immagine non può essere più lungo di 128 caratteri")]
+        [StartsWith("https://", "./img/", ErrorMessage = "L'URL provveduto deve iniziare con \"https://\" oppure \"./img/\"")]
+        [EndsWith(".png", ".jpg", ".jpeg", ".webp", ErrorMessage = "L'URL provveduto deve finire con"
+                                                                   + @""".png"", "".jpg"","".jpeg"" oppure "".webp""")]
         public string ImageUrl { get; set; }
 
         [Range(0, int.MaxValue)]
         public int DaysDuration { get; set; }
 
         [Range(0, int.MaxValue)]
-        public int Destinations { get ; set; }
+        public int Destinations { get; set; }
 
         [Range(0, double.MaxValue)]
         public double CostPerPerson { get; set; }
